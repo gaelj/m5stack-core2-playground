f10d3474e6118a50af4b309c34c02f8d09d7278c
Gael James <gaeljames@gmail.com>
2024-03-31 00:53:27 +0100

    ✨ Test display

--- M5StackCore2Playground/Playground1/Program.cs
+++ M5StackCore2Playground/Playground1/Program.cs
@@ -1,6 +1,20 @@
 using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.Threading;
+using nanoFramework.M5Core2;
+using nanoFramework.M5Stack;
+using nanoFramework.Runtime;
+using nanoFramework.Presentation;
+using nanoFramework.System;
+using nanoFramework.UI;
+using nanoFramework.Hardware;
+using nanoFramework.Hardware.Esp32;
+using nanoFramework.Hardware.Esp32.Touch;
+using nanoFramework.Presentation.Media;
+using nanoFramework.Presentation.Controls;
+using nanoFramework.Presentation.Shapes;
+using Console = nanoFramework.M5Stack.Console;
 
 namespace Playground1
 {
@@ -9,6 +23,22 @@ namespace Playground1
         public static void Main()
         {
             Debug.WriteLine("Hello from nanoFramework!");
+
+            M5Core2.InitializeScreen(2 * 1024 * 1024);
+
+            Console.Clear();
+
+            // Test the console display
+            Console.Write("This is a short text. ");
+            Console.ForegroundColor = Color.Red;
+            Console.WriteLine("This one displays in red after the previous one and is already at the next line.");
+            Console.BackgroundColor = Color.Yellow;
+            Console.ForegroundColor = Color.RoyalBlue;
+            Console.WriteLine("And this is blue on yellow background");
+            Console.ResetColor();
+            Console.CursorLeft = 0;
+            Console.CursorTop = 8;
+            Console.Write("This is white on black again and on 9th line");
 
             Thread.Sleep(Timeout.Infinite);
 
