4716ab45d99443aeba45a703481556b824000387
Gaël James <gaeljames@gmail.com>
2024-04-01 22:03:58 +0200

    ✨ Test vibrator and LED in a loop

--- M5StackCore2Playground/Playground1/Program.cs
+++ M5StackCore2Playground/Playground1/Program.cs
@@ -22,25 +22,47 @@ namespace Playground1
     {
         public static void Main()
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            // Debug.WriteLine("Hello from nanoFramework!");
 
             M5Core2.InitializeScreen(2 * 1024 * 1024);
+            M5Core2.InitializeScreen();
+            Thread.Sleep(1000);
+            // Screen.Enabled = true;
 
-            Console.Clear();
+            while (true) {
+
+                M5Core2.Vibrate = true;
+                M5Core2.PowerLed = true;
+
+                Thread.Sleep(1000);
+
+                M5Core2.Vibrate = false;
+                M5Core2.PowerLed = false;
+
+                Thread.Sleep(1000);
+
+            }
+
+            // Console.Clear();
 
             // Test the console display
-            Console.Write("This is a short text. ");
-            Console.ForegroundColor = Color.Red;
-            Console.WriteLine("This one displays in red after the previous one and is already at the next line.");
-            Console.BackgroundColor = Color.Yellow;
-            Console.ForegroundColor = Color.RoyalBlue;
-            Console.WriteLine("And this is blue on yellow background");
-            Console.ResetColor();
-            Console.CursorLeft = 0;
-            Console.CursorTop = 8;
-            Console.Write("This is white on black again and on 9th line");
+            // Console.Write("This is a short text. ");
+            // Console.ForegroundColor = Color.Red;
+            // Console.WriteLine("This one displays in red after the previous one and is already at the next line.");
+            // Console.BackgroundColor = Color.Yellow;
+            // Console.ForegroundColor = Color.RoyalBlue;
+            // Console.WriteLine("And this is blue on yellow background");
+            // Console.ResetColor();
+            // Console.CursorLeft = 0;
+            // Console.CursorTop = 8;
+            // Console.Write("This is white on black again and on 9th line");
 
-            Thread.Sleep(Timeout.Infinite);
+            // for (var i = 0; i < 100; i++)
+            // {
+            //     Console.WriteLine($"Line {i}");
+            // }
+
+            // Thread.Sleep(Timeout.Infinite);
 
             // Browse our samples repository: https://github.com/nanoframework/samples
             // Check our documentation online: https://docs.nanoframework.net/
